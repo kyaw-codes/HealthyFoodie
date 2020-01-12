e573be6a1f7dd151284e5c1572abd866adc4e6cb
ko kyaw <kyaw.codes@gmail.com>
2020-01-12 16:58:52 +0630

    Landing page one half way complete

--- app/src/main/java/com/kyaw/healthyfoodie/MainActivity.kt
+++ app/src/main/java/com/kyaw/healthyfoodie/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val fm = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/kyaw/healthyfoodie/ui/landing_pages/LandingFragmentTwo.kt
@@ -0,0 +1,26 @@
+package com.kyaw.healthyfoodie.ui.landing_pages
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.kyaw.healthyfoodie.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LandingFragmentTwo : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_landing_fragment_two, container, false)
+    }
+
+
+}
